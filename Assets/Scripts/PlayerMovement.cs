973fe5b291d3556467589e54f3f2ac5710ba3f3f
KamilMaliszewskiGameDev <kmaliszewskicontact@gmail.com>
2022-10-19 20:34:14 +0200

    changed movement + prefabs and prepared waypoints for platform movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,15 +6,18 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private const float JUMPING_FORCE = 5f;
     [SerializeField] private const float MOVEMENT_SPEED = 3f;
+    [SerializeField] LayerMask ground;
 
     private Rigidbody playerRigidbody;
+
+    [SerializeField] Transform groundCheck;
     // Start is called before the first frame update
     void Start()
     {
         playerRigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
+    // Update is called once per frame  
     void Update()
     {
         Walk();
@@ -22,7 +25,7 @@ public class PlayerMovement : MonoBehaviour
     }
 
     public void Jump(){
-        if(Input.GetButtonDown("Jump")){
+        if(Input.GetButtonDown("Jump") && isGrounded()){
             playerRigidbody.velocity = new Vector3(playerRigidbody.velocity.x, JUMPING_FORCE, playerRigidbody.velocity.z);
         }
     }
@@ -32,5 +35,9 @@ public class PlayerMovement : MonoBehaviour
         float verticalInput = Input.GetAxis("Vertical");
 
         playerRigidbody.velocity = new Vector3(horizontalInput * MOVEMENT_SPEED, playerRigidbody.velocity.y, verticalInput * MOVEMENT_SPEED);
+    }
+
+    public bool isGrounded(){
+        return Physics.CheckSphere(groundCheck.position, .1f, ground);
     }
 }
---
+++ Assets/Scripts/WaypointFollower.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointFollower : MonoBehaviour
+{
+
+    [SerializeField] GameObject[] waypoints;
+    [SerializeField] float speed = 1f;
+
+    int currentWaypointIndex = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position
+    }
+}
